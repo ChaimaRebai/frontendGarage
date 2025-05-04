1af3baee4ea66ba130988d89c007c973252c9b27
hechmi <hechmi@80-FA-5B-70-D0-ED.home>
2025-05-04 14:39:05 +0100

    ajouter logout

--- src/app/pageAcceuil/acceuil/acceuil.component.ts
+++ src/app/pageAcceuil/acceuil/acceuil.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from '../../auth/services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-acceuil',
   standalone: false,
   templateUrl: './acceuil.component.html',
-  styleUrl: './acceuil.component.css'
+  styleUrl: './acceuil.component.css',
 })
 export class AcceuilComponent {
+  loggedIn = false;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loggedIn = this.authService.isLoggedIn();
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+  
 }
